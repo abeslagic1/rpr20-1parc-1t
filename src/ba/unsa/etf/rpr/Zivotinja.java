fa3169ddac6241c808d43fb9faa59c0abf1295db
Amar Beslagic <abeslagic1@etf.unsa.ba>
2021-12-08 12:57:09 +0100

    Prolazi 6 testova

--- src/ba/unsa/etf/rpr/Zivotinja.java
+++ src/ba/unsa/etf/rpr/Zivotinja.java
@@ -8,16 +8,40 @@ public abstract class Zivotinja {
         //moze se koristit i metoda trim koja izbacuje praznine.
         if(id.trim().isBlank()) throw new IllegalArgumentException("Id ne moze biti prazno");
         if(ime.trim().isBlank()) throw new IllegalArgumentException("Ime ne moze biti prazno");
+        if(!provjeriId(id,ime)) throw new NeispravanFormatIdaException("Id nije ok");
         this.id = id;
         this.ime = ime;
+    }
+
+    private boolean provjeriId(String id, String ime) throws NeispravanFormatIdaException{
+        String novoIme = "";
+        for(int i=0; i<ime.length(); i++){
+            char c = ime.charAt(i);
+            if(c >= 'a' && c <= 'z') novoIme += c;
+            if(c >= 'A' && c <= 'Z') novoIme += (char)(c + 32);
+            if(c == 'č' || c == 'Č' || c == 'ć' || c == 'Ć') novoIme += 'c';
+            if(c == 'š' || c == 'Š') novoIme += 's';
+            if(c == 'đ' || c == 'Đ') novoIme += 'd';
+            if(c == 'ž' || c == 'Ž') novoIme += 'z';
+        }
+
+        if(id.length() <= novoIme.length()+1) throw new NeispravanFormatIdaException("Id je prekratak" + novoIme);
+        if(!id.substring(0, novoIme.length()).equals(novoIme)) throw new NeispravanFormatIdaException("Id se ne poklapa");
+        if(id.charAt(novoIme.length()) != '-') throw new NeispravanFormatIdaException("znak nije crtica");
+        for(int i = novoIme.length()+1; i < id.length(); i++){
+            if(id.charAt(i) < '0' || id.charAt(i) > '9') throw new NeispravanFormatIdaException("znak nije slovo"+novoIme);
+        }
+        return true;
     }
 
     public String getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(String id) throws NeispravanFormatIdaException{
         if(id.trim().isBlank()) throw new IllegalArgumentException("Id ne moze biti prazno");
+        if(!provjeriId(id,ime)) throw new NeispravanFormatIdaException("Id nije ok");
+
         this.id = id;
     }
 
@@ -25,8 +49,10 @@ public abstract class Zivotinja {
         return ime;
     }
 
-    public void setIme(String ime) {
+    public void setIme(String ime) throws NeispravanFormatIdaException{
         if(ime.trim().isBlank()) throw new IllegalArgumentException("Ime ne moze biti prazno");
+        if(!provjeriId(id,ime)) throw new NeispravanFormatIdaException("Id nije ok");
+
         this.ime = ime;
     }
 
